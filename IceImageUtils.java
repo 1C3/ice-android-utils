b4c1a40d5d34875533cf31e929305bf38ea737a8
1C3 <riccardo@ice.ovh>
2016-04-30 17:24:21 +0200

    Add async loading support

--- IceImageUtils.java
+++ IceImageUtils.java
@@ -1,8 +1,27 @@
+package ovh.ice.icecons;
+
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.lang.ref.WeakReference;
 
 public class IceImageUtils {
+
+    public static void bitmapLoadAsync( ImageView imageView, Resources res, int resId, int width, int height ) {
+
+        BitmapWorkerTask task = new BitmapWorkerTask( imageView );
+
+        task.res = res;
+        task.resId = resId;
+        task.width = width;
+        task.height = height;
+
+        task.execute( resId );
+    }
 
     public static Bitmap bitmapLoad( Resources res, int resId, int width, int height ) {
 
@@ -77,5 +96,50 @@ public class IceImageUtils {
         }
 
         return Bitmap.createBitmap( rawBitmap, cropX, cropY, cropWidth, cropHeight );
+    }
+}
+
+class BitmapWorkerTask extends AsyncTask< Integer, Void, Bitmap > {
+
+    private final WeakReference< ImageView > imageViewReference;
+
+    Resources res;
+    int resId, width, height;
+
+    public BitmapWorkerTask( ImageView imageView ) {
+
+        imageViewReference = new WeakReference<ImageView>(imageView);
+    }
+
+    // Decode image in background
+
+    @Override
+    protected Bitmap doInBackground( Integer... parameters ) {
+
+        return IceImageUtils.bitmapLoad( res, resId, width, height );
+    }
+
+    // Check if image view still exists and set bitmap
+
+    @Override
+    protected void onPostExecute( Bitmap bitmap ) {
+
+        if ( imageViewReference != null && bitmap != null ) {
+
+            final ImageView imageView = imageViewReference.get();
+
+            if ( imageView != null ) {
+
+                imageView.setImageBitmap(bitmap);
+
+                imageView.setAlpha( 0f );
+                imageView.setVisibility( View.VISIBLE );
+
+                imageView.animate()
+                        .alpha(1f)
+                        .setDuration( res.getInteger( android.R.integer.config_mediumAnimTime ) )
+                        .setListener( null );
+            }
+        }
     }
 }
